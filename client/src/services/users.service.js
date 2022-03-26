06d39e75c2bbe2ae57f13ba62e9d8125d8833d94
Ananda Khamkula <ananda.kha@gmail.com>
2022-03-26 12:58:17 +0000

    initial commit

---
+++ client/src/services/users.service.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const UsersService = {
+  http: axios.create({
+    //baseURL: 'http://localhost:3000/api/users',
+    baseURL: 'http://153.92.4.204:3000/api/users',
+  }),
+  get() {
+    return this.http.get()
+  },
+  post(userData) {
+    return this.http.post('', userData)
+  },
+  put(id, userData) {
+    return this.http.put(`/${id}`, userData)
+  },
+  delete(id) {
+    return this.http.delete(`/${id}`)
+  },
+}
---
+++ client/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  publicPath: '/client',
+  outputDir: '../dist',
+  devServer: {
+    host: '153.92.4.204',
+    //disableHostCheck: true,
+    //port: 8080,
+    //public: '0.0.0.0:8080/client'
+  },
+}
